0e17754500399009871d70977da316e2c1728e84
ahusseini18-ui <ahusseini18@britishschool.sch.ae>
2026-03-30 16:04:29 +0400

    Create tailwind.config.js file for Sugar Palm Club with pink and rose gold color scheme

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          DEFAULT: '#FF4D8B', // Main pink color
+          light: '#FF70A6', // Light pink
+        },
+        rosegold: {
+          DEFAULT: '#B76E79', // Main rose gold color
+          light: '#D2A39D', // Light rose gold
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
